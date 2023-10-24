a2b795ecfcc48262efc863a59843e315a467c7f9
Shunta Kainuma <arias230461@gmail.com>
2023-10-24 22:52:41 +0900

    プロジェクトの作成

---
+++ src/test/java/com/example/javakadai7/JavaKadai7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javakadai7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKadai7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
